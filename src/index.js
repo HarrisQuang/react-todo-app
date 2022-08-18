4960ac3711238fe42317a5e14524d600975ecf3a
kimhai <kimhaiareal@gmail.com>
2022-08-18 10:51:19 +0700

    P2

--- src/index.js
+++ src/index.js
@@ -6,4 +6,9 @@ import TodoContainer from "./components/TodoContainer"
 
 // ReactDOM.render(element, document.getElementById("root"))
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+ReactDOM.render(
+    <React.StrictMode>
+        <TodoContainer />
+    </React.StrictMode>, 
+    document.getElementById("root")
+)
