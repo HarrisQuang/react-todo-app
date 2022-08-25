cea3fa5c1066c41ef6aa577c081fe6d8f9c4ccc5
hquang2 <haikim.quang@dxc.com>
2022-08-25 18:03:49 +0700

    add css

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,13 @@
 import React from "react"
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
     render() {
-        return <li>
+        return <li className={styles.item}>
             {/* <input type="checkbox" checked={this.props.todo.completed} 
             onChange={() => console.log("clicked")}/>
             {this.props.todo.title} */}
-            <input type="checkbox" checked={this.props.todo.completed} 
+            <input type="checkbox" className={styles.checkbox} checked={this.props.todo.completed} 
             onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
             <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
             {this.props.todo.title}
